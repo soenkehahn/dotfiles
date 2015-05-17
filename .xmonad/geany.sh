f775985b6d0f3093193679646c21c714b41563ff
Sönke Hahn <soenkehahn@gmail.com>
2015-05-17 11:19:29 +0800

    xmonad: use Makefile

--- .xmonad/geany.sh
+++
@@ -1,5 +0,0 @@
-set -o errexit
-
-cabal exec ghc -- xmonad.hs -Wall -fno-warn-name-shadowing -fno-warn-missing-signatures -outputdir build -o xmonad-x86_64-linux -ilib
-ack-grep --haskell undefined && false
-# xmonad --restart
