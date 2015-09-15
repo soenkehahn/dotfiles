ffe551d221f9278651f6d6622b2b585f858d5bbd
Sönke Hahn <soenkehahn@gmail.com>
2015-09-15 10:24:54 +0800

    switch to chromium

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -108,7 +108,7 @@ myKeys conf =
     -- start programs
     ((modKey, xK_n), runOrRaisePrompt runOrRaiseConfig) :
     ((modKey, xK_r), spawn $ terminal conf) :
-    ((modKey, xK_t), spawn "firefox") :
+    ((modKey, xK_t), spawn "chromium-browser") :
     ((modKey, xK_d), spawn "dolphin") :
 
     -- switch between programs
