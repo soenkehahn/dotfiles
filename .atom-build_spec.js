dfb9e42e2a951d2366227b4892dcad0a595a40c5
Sönke Hahn <soenkehahn@gmail.com>
2017-02-16 16:41:52 -0500

    restructured atom geany tests

--- .atom-build_spec.js
+++ .atom-build_spec.js
@@ -22,107 +22,111 @@ child_process.execSync('touch client/tests/client-test.file');
 
 describe('.atom-build.js', () => {
   describe('functionMatchFoo', () => {
-    it('matches gcc style messages', () => {
-      const output = [
-        "./file/foo:13:8: message",
-        "something"
-      ].join('\n');
-      expected = [{
-        file: "./file/foo",
-        line: 13,
-        col: 8
-      }];
-      expectMatches(output, expected);
-    });
-    it('matches gcc style messages without column', () => {
-      const output = [
-        "./file/foo:13: message",
-        "something"
-      ].join('\n');
-      expected = [{
-        file: "./file/foo",
-        line: 13
-      }];
-      expectMatches(output, expected);
-    });
-    it('matches weird file names', () => {
-      const output = "./file/foo-Bar_baz-123.exe:42: message\n";
-      expected = [{
-        file: "./file/foo-Bar_baz-123.exe",
-        line: 42
-      }];
-      expectMatches(output, expected);
-
-    });
-    it('matches elm error messages', () => {
-      const output = [
-        "something",
-        "-- TYPE MISMATCH ------------------------------------- file/Operational.elm",
-        "",
-        "The 4th argument to function `simulate` is causing a mismatch.",
-        "",
-        "73|                         simulate program",
-      ].join('\n');
-      expected = [{
-        file: "file/Operational.elm",
-        line: 73
-      }];
-      expectMatches(output, expected);
-    });
-    it('matches (uncolorized) hspec messages', () => {
-      const output = "  file/foo:85:";
-      expected = [{
-        file: "file/foo",
-        line: 85
-      }];
-      expectMatches(output, expected);
-    });
-    it('matches earlier messages first (reversed)', () => {
-      const output = [
-        "something",
-        "-- TYPE MISMATCH ------------------------------------- file/Operational.elm",
-        "",
-        "The 4th argument to function `simulate` is causing a mismatch.",
-        "",
-        "73|                         simulate program",
-        "",
-        "./file/foo.txt:42:"
-      ].join('\n');
-      expected = [
-        {
-          file: "./file/foo.txt",
+    describe('error matching', () => {
+      it('matches gcc style messages', () => {
+        const output = [
+          "./file/foo:13:8: message",
+          "something"
+        ].join('\n');
+        expected = [{
+          file: "./file/foo",
+          line: 13,
+          col: 8
+        }];
+        expectMatches(output, expected);
+      });
+      it('matches gcc style messages without column', () => {
+        const output = [
+          "./file/foo:13: message",
+          "something"
+        ].join('\n');
+        expected = [{
+          file: "./file/foo",
+          line: 13
+        }];
+        expectMatches(output, expected);
+      });
+      it('matches weird file names', () => {
+        const output = "./file/foo-Bar_baz-123.exe:42: message\n";
+        expected = [{
+          file: "./file/foo-Bar_baz-123.exe",
           line: 42
-        },
-        {
+        }];
+        expectMatches(output, expected);
+
+      });
+      it('matches elm error messages', () => {
+        const output = [
+          "something",
+          "-- TYPE MISMATCH ------------------------------------- file/Operational.elm",
+          "",
+          "The 4th argument to function `simulate` is causing a mismatch.",
+          "",
+          "73|                         simulate program",
+        ].join('\n');
+        expected = [{
           file: "file/Operational.elm",
           line: 73
-        }
-      ];
-      expectMatches(output, expected);
+        }];
+        expectMatches(output, expected);
+      });
+      it('matches (uncolorized) hspec messages', () => {
+        const output = "  file/foo:85:";
+        expected = [{
+          file: "file/foo",
+          line: 85
+        }];
+        expectMatches(output, expected);
+      });
+      it('matches earlier messages first (reversed)', () => {
+        const output = [
+          "something",
+          "-- TYPE MISMATCH ------------------------------------- file/Operational.elm",
+          "",
+          "The 4th argument to function `simulate` is causing a mismatch.",
+          "",
+          "73|                         simulate program",
+          "",
+          "./file/foo.txt:42:"
+        ].join('\n');
+        expected = [
+          {
+            file: "./file/foo.txt",
+            line: 42
+          },
+          {
+            file: "file/Operational.elm",
+            line: 73
+          }
+        ];
+        expectMatches(output, expected);
+      });
     });
-    it('removes file locations of non-existing files', () => {
-      const output = "./does-not-exist.file:42:\n"
-      expectMatches(output, []);
-    });
-    it("also searches in subdirectory 'tests'", () => {
-      const output = "./test.file:42:\n";
-      expected = [
-        {
-          file: "tests/test.file",
-          line: 42
-        }
-      ];
-      expectMatches(output, expected);
-    });
-  it("also searches in subdirectory 'client/tests'", () => {
-      const output = "./client-test.file:42:\n";
-      expected = [
-        {
-          file: "client/tests/client-test.file",
-          line: 42
-        }
-      ];
-      expectMatches(output, expected);
+    describe('file locations', () => {
+      it('removes file locations of non-existing files', () => {
+        const output = "./does-not-exist.file:42:\n"
+        expectMatches(output, []);
+      });
+      it("also searches in subdirectory 'tests'", () => {
+        const output = "./test.file:42:\n";
+        expected = [
+          {
+            file: "tests/test.file",
+            line: 42
+          }
+        ];
+        expectMatches(output, expected);
+      });
+      it("also searches in subdirectory 'client/tests'", () => {
+        const output = "./client-test.file:42:\n";
+        expected = [
+          {
+            file: "client/tests/client-test.file",
+            line: 42
+          }
+        ];
+        expectMatches(output, expected);
+      });
     });
   });
 });
