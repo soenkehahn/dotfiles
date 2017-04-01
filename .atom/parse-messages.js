37588a9e56adf4c0ceafe8236732adc9eb4b0390
Sönke Hahn <soenkehahn@gmail.com>
2017-04-01 17:40:47 -0400

    add message parsing files (not used)

---
+++ .atom/parse-messages.js
@@ -0,0 +1,54 @@
+XRegExp = require('xregexp');
+fs = require('fs');
+path = require('path');
+
+_functionMatch = function (output) {
+  filePattern = '(?<file>[a-zA-Z-_\\d\.\/]+)';
+  linePattern = '(?<line>\\d+)';
+  colPattern = '(?<col>\\d+)';
+  patterns = [
+    XRegExp(`^ *${filePattern}:${linePattern}(:(?<col>\\d+):)?`),
+    XRegExp(`^-- .+ -+ ${filePattern}\\n\\n(?<message>.+)\\n\\n( )?${linePattern}\\|`),
+    XRegExp(`\\sat Context\.<anonymous> \\(${filePattern}:${linePattern}:(?<col>\\d+)\\)`),
+    XRegExp(`  ✗ ${filePattern}\\n     ✗ #${linePattern}: `),
+    XRegExp(`      at ${filePattern}:${linePattern}:${colPattern}`),
+  ];
+  var locations = [];
+  lines = output.split('\n');
+  lines.forEach((line, index) => {
+    patterns.forEach(pattern => {
+      section = lines.slice(index, index + 5).join('\n');
+      const match = XRegExp.exec(section, pattern);
+      if (match) {
+        match.line = parseInt(match.line);
+        loc = {
+          file: match.file,
+          line: match.line
+        };
+        if (typeof match.col !== "undefined"){
+          loc.col = parseInt(match.col);
+        }
+        locations.push(loc);
+      }
+    });
+  });
+  locations = locations.filter(location => {
+    if (fs.existsSync(location.file)) {
+      return true;
+    } else if (fs.existsSync('tests/' + location.file)) {
+      location.file = path.normalize("tests/" + location.file);
+      return true;
+    } else if (fs.existsSync('client/tests/' + location.file)) {
+      location.file = path.normalize("client/tests/" + location.file);
+      return true;
+    };
+    return false;
+  });
+  return locations;
+};
+
+module.exports = {
+  cmd: "make -f geany",
+  functionMatch: _functionMatch,
+  _functionMatch: _functionMatch
+};
---
+++ .atom/parse-messages_spec.js
@@ -0,0 +1,176 @@
+const chai = require('chai');
+const expect = chai.expect;
+const child_process = require('child_process');
+const _ = require('lodash');
+
+const atomBuild = require('./.atom-build');
+
+function expectMatches(output, expected) {
+  result = atomBuild._functionMatch(output);
+  expect(result).to.eql(expected);
+}
+
+process.chdir('/tmp');
+child_process.execSync('mkdir -p file');
+child_process.execSync('touch file/foo');
+child_process.execSync('touch file/foo.txt');
+child_process.execSync('touch file/foo-Bar_baz-123.exe');
+child_process.execSync('touch file/Operational.elm');
+child_process.execSync('mkdir -p tests');
+child_process.execSync('touch tests/test.file');
+child_process.execSync('mkdir -p client/tests');
+child_process.execSync('touch client/tests/client-test.file');
+
+describe('.atom-build.js', () => {
+  describe('functionMatchFoo', () => {
+    describe('error matching', () => {
+      it('matches gcc style messages', () => {
+        const output = [
+          "./file/foo:13:8: message",
+          "something"
+        ].join('\n');
+        expected = [{
+          file: "./file/foo",
+          line: 13,
+          col: 8
+        }];
+        expectMatches(output, expected);
+      });
+      it('matches gcc style messages without column', () => {
+        const output = [
+          "./file/foo:13: message",
+          "something"
+        ].join('\n');
+        expected = [{
+          file: "./file/foo",
+          line: 13
+        }];
+        expectMatches(output, expected);
+      });
+      it('matches weird file names', () => {
+        const output = "./file/foo-Bar_baz-123.exe:42: message\n";
+        expected = [{
+          file: "./file/foo-Bar_baz-123.exe",
+          line: 42
+        }];
+        expectMatches(output, expected);
+
+      });
+      it('matches elm error messages', () => {
+        const output = [
+          "something",
+          "-- TYPE MISMATCH ------------------------------------- file/Operational.elm",
+          "",
+          "The 4th argument to function `simulate` is causing a mismatch.",
+          "",
+          "73|                         simulate program",
+        ].join('\n');
+        expected = [{
+          file: "file/Operational.elm",
+          line: 73
+        }];
+        expectMatches(output, expected);
+      });
+      it('matches (uncolorized) hspec messages', () => {
+        const output = "  file/foo:85:";
+        expected = [{
+          file: "file/foo",
+          line: 85
+        }];
+        expectMatches(output, expected);
+      });
+      describe('matches mocha test failures', () => {
+        _.forEach(['      ', '    '], (spaces) => {
+          it(`with ${spaces.length} leading spaces`, () => {
+            const output = spaces + "at Context.<anonymous> (file/foo:85:23)";
+            expected = [{
+              file: "file/foo",
+              line: 85,
+              col: 23
+            }];
+            expectMatches(output, expected);
+          });
+        });
+      });
+      it('matches coffeelint errors', () => {
+        const output = [
+          "  ✗ file/foo",
+          "     ✗ #201: Unnecessary double quotes are forbidden.",
+        ].join('\n');
+        expected = [{
+          file: "file/foo",
+          line: 201,
+        }];
+        expectMatches(output, expected);
+      });
+      it('matches node exceptions', () => {
+        const output = '      at file/foo:26:30';
+        expected = [{
+          file: "file/foo",
+          line: 26,
+          col: 30,
+        }];
+        expectMatches(output, expected);
+      });
+      it('matches flow errors', () => {
+        const output = 'file/foo:26';
+        expected = [{
+          file: "file/foo",
+          line: 26,
+        }];
+        expectMatches(output, expected);
+      });
+      describe('message order', () => {
+        it('matches earlier messages first', () => {
+          const output = [
+            "something",
+            "-- TYPE MISMATCH ------------------------------------- file/Operational.elm",
+            "",
+            "The 4th argument to function `simulate` is causing a mismatch.",
+            "",
+            "73|                         simulate program",
+            "",
+            "./file/foo.txt:42:"
+          ].join('\n');
+          expected = [
+            {
+              file: "file/Operational.elm",
+              line: 73
+            },
+            {
+              file: "./file/foo.txt",
+              line: 42
+            },
+          ];
+          expectMatches(output, expected);
+        });
+      });
+    });
+    describe('file locations', () => {
+      it('removes file locations of non-existing files', () => {
+        const output = "./does-not-exist.file:42:\n"
+        expectMatches(output, []);
+      });
+      it("also searches in subdirectory 'tests'", () => {
+        const output = "./test.file:42:\n";
+        expected = [
+          {
+            file: "tests/test.file",
+            line: 42
+          }
+        ];
+        expectMatches(output, expected);
+      });
+      it("also searches in subdirectory 'client/tests'", () => {
+        const output = "./client-test.file:42:\n";
+        expected = [
+          {
+            file: "client/tests/client-test.file",
+            line: 42
+          }
+        ];
+        expectMatches(output, expected);
+      });
+    });
+  });
+});
