c8815519787b24eb39b1f8834a3f2944fecbc96d
soenkehahn <SoenkeHahn@gmail.com>
2017-09-19 12:07:54 -0400

    prettier police

--- .atom/init.js
+++ .atom/init.js
@@ -1,10 +1,8 @@
-
 // @flow
 
-const child_process = require('child_process')
-const fs = require('fs')
-const {parseMessages} = require('./parse-messages')
-
+const child_process = require("child_process");
+const fs = require("fs");
+const { parseMessages } = require("./parse-messages");
 
 /*::
 declare var atom: {|
@@ -25,37 +23,37 @@ declare var atom: {|
 |}
 */
 
-const path = require('path')
+const path = require("path");
 
 atom.workspace.observeTextEditors(editor => {
-  if (path.basename(editor.getPath()) === 'Makefile') {
-    editor.setSoftTabs(false)
+  if (path.basename(editor.getPath()) === "Makefile") {
+    editor.setSoftTabs(false);
   }
-  if (path.extname(editor.getPath()) === '.mk') {
-    editor.setSoftTabs(false)
+  if (path.extname(editor.getPath()) === ".mk") {
+    editor.setSoftTabs(false);
   }
-  if (path.basename(editor.getPath()) === 'geany') {
-    editor.setSoftTabs(false)
+  if (path.basename(editor.getPath()) === "geany") {
+    editor.setSoftTabs(false);
   }
-})
+});
 
-atom.commands.add('atom-text-editor', {
-  'custom:save-and-exit-insert-mode': event => {
-    const editor = atom.workspace.getActiveTextEditor()
-    editor.save()
-    const editorView = atom.views.getView(editor)
-    atom.commands.dispatch(editorView, 'vim-mode-plus:activate-normal-mode')
-  },
-})
+atom.commands.add("atom-text-editor", {
+  "custom:save-and-exit-insert-mode": event => {
+    const editor = atom.workspace.getActiveTextEditor();
+    editor.save();
+    const editorView = atom.views.getView(editor);
+    atom.commands.dispatch(editorView, "vim-mode-plus:activate-normal-mode");
+  }
+});
 
-atom.commands.add('atom-text-editor', {
+atom.commands.add("atom-text-editor", {
   "custom:save-and-test": event => {
-    const editor = atom.workspace.getActiveTextEditor()
-    editor.save()
-    const editorView = atom.views.getView(editor)
-    atom.commands.dispatch(editorView, "tertestrial:test-file")
-  },
-})
+    const editor = atom.workspace.getActiveTextEditor();
+    editor.save();
+    const editorView = atom.views.getView(editor);
+    atom.commands.dispatch(editorView, "tertestrial:test-file");
+  }
+});
 
 // * geany
 
@@ -69,27 +67,28 @@ type Loc = {|
 
 */
 
-let geanMessages /*: Array<Loc> */ = []
-let geanIndex = 0
+let geanMessages /*: Array<Loc> */ = [];
+let geanIndex = 0;
 
-atom.commands.add('atom-text-editor', {
-  'custom:read-gean': event => {
-    const projectDir = atom.project.getPaths()[0]
-    const output = fs.readFileSync(projectDir + '/gean', {encoding: 'utf-8'})
-    geanMessages = parseMessages(output)
-    geanIndex = 0
+atom.commands.add("atom-text-editor", {
+  "custom:read-gean": event => {
+    const projectDir = atom.project.getPaths()[0];
+    const output = fs.readFileSync(projectDir + "/gean", { encoding: "utf-8" });
+    geanMessages = parseMessages(output);
+    geanIndex = 0;
   }
-})
+});
 
-atom.commands.add('atom-text-editor', {
-  'custom:next-gean': event => {
+atom.commands.add("atom-text-editor", {
+  "custom:next-gean": event => {
+    console.log("next-gean");
     if (geanIndex < geanMessages.length) {
-      const loc = geanMessages[geanIndex]
+      const loc = geanMessages[geanIndex];
       atom.workspace.open(loc.file, {
         initialLine: loc.line - 1,
-        initialColumn: loc.col,
-      })
+        initialColumn: loc.col
+      });
     }
-    geanIndex++
+    geanIndex++;
   }
-})
+});
