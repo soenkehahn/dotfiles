b2365222ac2992235db523979cb075261994655d
Sönke Hahn <soenkehahn@gmail.com>
2017-09-23 18:29:20 -0400

    prettier police

--- .atom/parse-messages.js
+++ .atom/parse-messages.js
@@ -1,8 +1,8 @@
 // @flow
 
-const XRegExp = require('xregexp');
-const fs = require('fs');
-const path = require('path');
+const XRegExp = require("xregexp");
+const fs = require("fs");
+const path = require("path");
 
 /*::
 
@@ -20,40 +20,44 @@ type Loc = {|
 */
 
 function combinePaths(...snippets /*: Array<string> */) /*: string */ {
-  let result = snippets.pop()
+  let result = snippets.pop();
   snippets.forEach(snippet => {
-    if (! path.isAbsolute(result)) {
-      result = snippet + '/' + result
+    if (!path.isAbsolute(result)) {
+      result = snippet + "/" + result;
     }
-  })
-  return path.normalize(result)
+  });
+  return path.normalize(result);
 }
 
 const parseMessages = function(messages /*: string */) /*: Array<Loc> */ {
-  const filePattern = '(?<file>[a-zA-Z-_\\d\.\/]+)';
-  const linePattern = '(?<line>\\d+)';
-  const colPattern = '(?<col>\\d+)';
+  const filePattern = "(?<file>[a-zA-Z-_\\d./]+)";
+  const linePattern = "(?<line>\\d+)";
+  const colPattern = "(?<col>\\d+)";
   const patterns = [
     XRegExp(`^ *${filePattern}:${linePattern}(:${colPattern}:)?`),
     XRegExp(`^Error: ${filePattern}:${linePattern}`),
-    XRegExp(`^-- .+ -+ ${filePattern}\\n\\n(?<message>.+)\\n\\n( )?${linePattern}\\|`),
-    XRegExp(`\\sat Context\.<anonymous> \\(${filePattern}:${linePattern}:(?<col>\\d+)\\)`),
+    XRegExp(
+      `^-- .+ -+ ${filePattern}\\n\\n(?<message>.+)\\n\\n( )?${linePattern}\\|`
+    ),
+    XRegExp(
+      `\\sat Context\.<anonymous> \\(${filePattern}:${linePattern}:(?<col>\\d+)\\)`
+    ),
     XRegExp(`  ✗ ${filePattern}\\n     ✗ #${linePattern}: `),
-    XRegExp(`      at ${filePattern}:${linePattern}:${colPattern}`),
+    XRegExp(`      at ${filePattern}:${linePattern}:${colPattern}`)
   ];
   var locations = [];
-  const lines = messages.split('\n');
+  const lines = messages.split("\n");
   lines.forEach((line, index) => {
     patterns.forEach(pattern => {
-      const section = lines.slice(index, index + 5).join('\n');
+      const section = lines.slice(index, index + 5).join("\n");
       const match = XRegExp.exec(section, pattern);
       if (match) {
         match.line = parseInt(match.line);
         const loc /*: Loc */ = {
           file: match.file,
-          line: match.line,
+          line: match.line
         };
-        if (typeof match.col !== "undefined"){
+        if (typeof match.col !== "undefined") {
           loc.col = parseInt(match.col);
         }
         locations.push(loc);
@@ -63,19 +67,19 @@ const parseMessages = function(messages /*: string */) /*: Array<Loc> */ {
   locations = locations.filter(location => {
     const candidates = [
       location.file,
-      combinePaths('tests', location.file),
-      combinePaths('client/tests', location.file),
-    ]
-    const projectDir = atom.project.getPaths()[0]
-    let exists = false
+      combinePaths("tests", location.file),
+      combinePaths("client/tests", location.file)
+    ];
+    const projectDir = atom.project.getPaths()[0];
+    let exists = false;
     candidates.forEach(candidate => {
       if (fs.existsSync(combinePaths(projectDir, candidate))) {
-        location.file = candidate
-        exists = true
+        location.file = candidate;
+        exists = true;
       }
-    })
+    });
     if (!exists) {
-      console.log('does not exist: ' + location.file);
+      console.log("does not exist: " + location.file);
     }
     return exists;
   });
--- .atom/parse-messages_spec.js
+++ .atom/parse-messages_spec.js
@@ -1,14 +1,14 @@
 // @flow
 
-const chai = require('chai');
+const chai = require("chai");
 const expect = chai.expect;
-const mocha = require('mocha');
+const mocha = require("mocha");
 const describe = mocha.describe;
 const it = mocha.it;
-const child_process = require('child_process');
-const _ = require('lodash');
+const child_process = require("child_process");
+const _ = require("lodash");
 
-const atomBuild = require('./parse-messages');
+const atomBuild = require("./parse-messages");
 
 function expectMatches(output, expected) {
   const result = atomBuild.parseMessages(output);
@@ -17,128 +17,139 @@ function expectMatches(output, expected) {
 
 global.atom = {
   project: {
-    getPaths: () => ['./.']
+    getPaths: () => ["./."]
   }
-}
+};
 
-process.chdir('/tmp');
-child_process.execSync('mkdir -p file');
-child_process.execSync('touch file/foo');
-child_process.execSync('touch file/foo.js');
-child_process.execSync('touch file/foo.txt');
-child_process.execSync('touch file/foo-Bar_baz-123.exe');
-child_process.execSync('touch file/Operational.elm');
-child_process.execSync('mkdir -p tests');
-child_process.execSync('touch tests/test.file');
-child_process.execSync('mkdir -p client/tests');
-child_process.execSync('touch client/tests/client-test.file');
+process.chdir("/tmp");
+child_process.execSync("mkdir -p file");
+child_process.execSync("touch file/foo");
+child_process.execSync("touch file/foo.js");
+child_process.execSync("touch file/foo.txt");
+child_process.execSync("touch file/foo-Bar_baz-123.exe");
+child_process.execSync("touch file/Operational.elm");
+child_process.execSync("mkdir -p tests");
+child_process.execSync("touch tests/test.file");
+child_process.execSync("mkdir -p client/tests");
+child_process.execSync("touch client/tests/client-test.file");
 
-describe('parse-messages.js', () => {
-  describe('parseMessages', () => {
-    describe('error matching', () => {
-      it('matches gcc style messages', () => {
-        const output = [
-          "./file/foo:13:8: message",
-          "something"
-        ].join('\n');
-        const expected = [{
-          file: "./file/foo",
-          line: 13,
-          col: 8
-        }];
+describe("parse-messages.js", () => {
+  describe("parseMessages", () => {
+    describe("error matching", () => {
+      it("matches gcc style messages", () => {
+        const output = ["./file/foo:13:8: message", "something"].join("\n");
+        const expected = [
+          {
+            file: "./file/foo",
+            line: 13,
+            col: 8
+          }
+        ];
         expectMatches(output, expected);
       });
-      it('matches gcc style messages without column', () => {
-        const output = [
-          "./file/foo:13: message",
-          "something"
-        ].join('\n');
-        const expected = [{
-          file: "./file/foo",
-          line: 13
-        }];
+      it("matches gcc style messages without column", () => {
+        const output = ["./file/foo:13: message", "something"].join("\n");
+        const expected = [
+          {
+            file: "./file/foo",
+            line: 13
+          }
+        ];
         expectMatches(output, expected);
       });
-      it('matches weird file names', () => {
+      it("matches weird file names", () => {
         const output = "./file/foo-Bar_baz-123.exe:42: message\n";
-        const expected = [{
-          file: "./file/foo-Bar_baz-123.exe",
-          line: 42
-        }];
+        const expected = [
+          {
+            file: "./file/foo-Bar_baz-123.exe",
+            line: 42
+          }
+        ];
         expectMatches(output, expected);
-
       });
-      it('matches elm error messages', () => {
+      it("matches elm error messages", () => {
         const output = [
           "something",
           "-- TYPE MISMATCH ------------------------------------- file/Operational.elm",
           "",
           "The 4th argument to function `simulate` is causing a mismatch.",
           "",
-          "73|                         simulate program",
-        ].join('\n');
-        const expected = [{
-          file: "file/Operational.elm",
-          line: 73
-        }];
+          "73|                         simulate program"
+        ].join("\n");
+        const expected = [
+          {
+            file: "file/Operational.elm",
+            line: 73
+          }
+        ];
         expectMatches(output, expected);
       });
-      it('matches (uncolorized) hspec messages', () => {
+      it("matches (uncolorized) hspec messages", () => {
         const output = "  file/foo:85:";
-        const expected = [{
-          file: "file/foo",
-          line: 85
-        }];
+        const expected = [
+          {
+            file: "file/foo",
+            line: 85
+          }
+        ];
         expectMatches(output, expected);
       });
 
-      describe('matches mocha test failures', () => {
-        _.forEach(['      ', '    '], (spaces) => {
+      describe("matches mocha test failures", () => {
+        _.forEach(["      ", "    "], spaces => {
           it(`with ${spaces.length} leading spaces`, () => {
             const output = spaces + "at Context.<anonymous> (file/foo:85:23)";
-            const expected = [{
-              file: "file/foo",
-              line: 85,
-              col: 23
-            }];
+            const expected = [
+              {
+                file: "file/foo",
+                line: 85,
+                col: 23
+              }
+            ];
             expectMatches(output, expected);
           });
         });
       });
 
-      it('matches coffeelint errors', () => {
+      it("matches coffeelint errors", () => {
         const output = [
           "  ✗ file/foo",
-          "     ✗ #201: Unnecessary double quotes are forbidden.",
-        ].join('\n');
-        const expected = [{
-          file: "file/foo",
-          line: 201,
-        }];
+          "     ✗ #201: Unnecessary double quotes are forbidden."
+        ].join("\n");
+        const expected = [
+          {
+            file: "file/foo",
+            line: 201
+          }
+        ];
         expectMatches(output, expected);
       });
 
-      it('matches node exceptions', () => {
-        const output = '      at file/foo:26:30';
-        const expected = [{
-          file: "file/foo",
-          line: 26,
-          col: 30,
-        }];
+      it("matches node exceptions", () => {
+        const output = "      at file/foo:26:30";
+        const expected = [
+          {
+            file: "file/foo",
+            line: 26,
+            col: 30
+          }
+        ];
         expectMatches(output, expected);
       });
 
-      it('matches flow errors', () => {
-        const output = 'Error: file/foo.js:47';
-        const expected = [{
-          file: "file/foo.js",
-          line: 47,
-        }];
+      it("matches flow errors", () => {
+        const output = "Error: file/foo.js:47";
+        const expected = [
+          {
+            file: "file/foo.js",
+            line: 47
+          }
+        ];
         expectMatches(output, expected);
       });
 
-      describe('message order', () => {
-        it('matches earlier messages first', () => {
+      describe("message order", () => {
+        it("matches earlier messages first", () => {
           const output = [
             "something",
             "-- TYPE MISMATCH ------------------------------------- file/Operational.elm",
@@ -148,7 +159,7 @@ describe('parse-messages.js', () => {
             "73|                         simulate program",
             "",
             "./file/foo.txt:42:"
-          ].join('\n');
+          ].join("\n");
           const expected = [
             {
               file: "file/Operational.elm",
@@ -157,19 +168,19 @@ describe('parse-messages.js', () => {
             {
               file: "./file/foo.txt",
               line: 42
-            },
+            }
           ];
           expectMatches(output, expected);
         });
       });
     });
-    describe('file locations', () => {
-      it('removes file locations of non-existing files', () => {
-        const output = "./does-not-exist.file:42:\n"
+    describe("file locations", () => {
+      it("removes file locations of non-existing files", () => {
+        const output = "./does-not-exist.file:42:\n";
         expectMatches(output, []);
       });
-      it('works for absolute files', () => {
-        const cwd = process.cwd()
+      it("works for absolute files", () => {
+        const cwd = process.cwd();
         const output = cwd + "/file/foo.txt:42:\n";
         const expected = [
           {
@@ -178,7 +189,7 @@ describe('parse-messages.js', () => {
           }
         ];
         expectMatches(output, expected);
-      })
+      });
       it("also searches in subdirectory 'tests'", () => {
         const output = "./test.file:42:\n";
         const expected = [
