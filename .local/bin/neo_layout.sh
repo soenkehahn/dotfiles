df871fbdaca1a0617ab866713e63f766b6637d07
soenkehahn <SoenkeHahn@gmail.com>
2017-11-21 21:42:20 -0500

    allow to set hostname through the environment in neo_layout.sh

--- .local/bin/neo_layout.sh
+++ .local/bin/neo_layout.sh
@@ -8,6 +8,10 @@ sleep 1
 scriptDir=$(dirname $0)
 localDir="$scriptDir/.."
 
+if [[ -z "${HOSTNAME}" ]]; then
+  HOSTNAME="$(hostname)"
+fi
+
 setxkbmap lv
-xmodmap "$localDir/neo_de.$(hostname).xmodmap"
+xmodmap "$localDir/neo_de.$HOSTNAME.xmodmap"
 xset -r 51
