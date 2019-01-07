84cfffc27ecee83358df0264c15945e7f35ea1a2
Sönke Hahn <soenkehahn@gmail.com>
2019-01-07 00:05:09 -0500

    install rustup

--- .local/custom/InstallStuff.hs
+++ .local/custom/InstallStuff.hs
@@ -12,9 +12,16 @@ main :: IO ()
 main = do
   homeDir <- getHomeDirectory
   shakeArgs shakeOptions $ do
-    let parcelExecutable = homeDir </> ".local/bin/parcel"
-    want [parcelExecutable]
 
-    parcelExecutable %> \ _ -> do
+    (homeDir </> ".local/bin/parcel") %>> do
       cmd "yarn global add parcel-bundler --prefix"
         (homeDir </> ".local")
+
+    (homeDir </> ".cargo/bin/rustup") %>> do
+      cmd Shell
+        "curl https://sh.rustup.rs -sSf | sh -s -- --no-modify-path -y"
+
+(%>>) :: FilePath -> Action () -> Rules ()
+executablePath %>> rule = do
+  want [executablePath]
+  executablePath %> \ _ -> rule
