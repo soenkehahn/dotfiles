4f108b65634cdefde9fb3257040f27fc871f826a
Sönke Hahn <soenkehahn@gmail.com>
2019-05-27 12:20:46 -0400

    tweak nix build script

--- .local/shahn_nix_executables/build.sh
+++ .local/shahn_nix_executables/build.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/env bash
 
+set -o errexit
+
 echo building nix executables...
 
 . ~/.nix-profile/etc/profile.d/nix.sh
 NIX_PATH=
-nice -n 19 nix-build -j4
+nice -n 19 nix-build --keep-going --max-jobs 4
