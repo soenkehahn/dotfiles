a515af02330c85927d7b580b8df50ab7c460e756
Sönke Hahn <soenkehahn@gmail.com>
2019-07-09 12:48:28 -0400

    update prettier

--- .local/shahn_nix_executables/default.nix
+++ .local/shahn_nix_executables/default.nix
@@ -1,35 +1,39 @@
-let
-  allPkgs = (import ./nixpkgs.nix);
-  pkgs = allPkgs.stable;
-in pkgs.buildEnv {
+with (import ./nixpkgs.nix);
+stable.buildEnv {
   name = "shahn_nix_executables";
-  paths = (with pkgs; [
-    ag
-    blackbox
-    cabal2nix
-    docker-compose
-    fd
-    fzf
-    ipfs
-    ngrok
-    nodejs
-    nodePackages.parcel-bundler
-    nodePackages.prettier
-    pandoc
-    terraform
-    yarn
-
-    allPkgs.unstable.just
-
-    (import ./atom.nix)
-    (import ./cargo-bump.nix)
-    (import ./cargo-script.nix)
-    (import ./el.nix)
-    (import ./i3-lock-and-suspend.nix)
-    (import ./i3-pretty-tree.nix)
-    (import ./rustup.nix)
-    (import ./sensei.nix)
-    (import ./sl.nix)
-    (import ./toast.nix)
-  ]);
+  paths = (
+    (with stable; [
+      ag
+      blackbox
+      cabal2nix
+      docker-compose
+      fd
+      fzf
+      ipfs
+      ngrok
+      nodejs
+      nodePackages.parcel-bundler
+      pandoc
+      terraform
+      yarn
+    ]) ++
+    [
+      unstable.just
+    ] ++
+    (with (np "e3190b2f9ec" "0x39sb1rrl5knzhn65558y28315sg77725hw09xn5lxil63b6mgl"); [
+      nodePackages.prettier
+    ]) ++
+    [
+      (import ./atom.nix)
+      (import ./cargo-bump.nix)
+      (import ./cargo-script.nix)
+      (import ./el.nix)
+      (import ./i3-lock-and-suspend.nix)
+      (import ./i3-pretty-tree.nix)
+      (import ./rustup.nix)
+      (import ./sensei.nix)
+      (import ./sl.nix)
+      (import ./toast.nix)
+    ]
+  );
 }
--- .local/shahn_nix_executables/nixpkgs.nix
+++ .local/shahn_nix_executables/nixpkgs.nix
@@ -1,16 +1,17 @@
+let
+  np = commit : sha256 :
+    let
+      pkgsSrc = builtins.fetchTarball {
+        url = "https://github.com/NixOS/nixpkgs/archive/${commit}.tar.gz";
+        sha256 =
+          if sha256 == null || sha256 == ""
+            then "0000000000000000000000000000000000000000000000000000"
+            else sha256;
+      };
+    in import pkgsSrc {};
+in
 {
-  stable =
-    let
-      pkgsSrc = builtins.fetchTarball {
-        url = "https://github.com/NixOS/nixpkgs/archive/3ddd23719bb.tar.gz";
-        sha256 = "sha256:1q8ghjkjnsrwi9v2iwkvaszbvmq1np5yjjrpf1y6gx25lk43ahbn";
-      };
-    in import pkgsSrc {};
-  unstable =
-    let
-      pkgsSrc = builtins.fetchTarball {
-        url = "https://github.com/NixOS/nixpkgs/archive/83ba5afcc96.tar.gz";
-        sha256 = "sha256:0swh1i3rm8a7fij6drz11s5nyzr145yh4n17k0572pp8knpxw762";
-      };
-    in import pkgsSrc {};
+  stable = np "3ddd23719bb" "1q8ghjkjnsrwi9v2iwkvaszbvmq1np5yjjrpf1y6gx25lk43ahbn";
+  unstable = np "83ba5afcc96" "0swh1i3rm8a7fij6drz11s5nyzr145yh4n17k0572pp8knpxw762";
+  inherit np;
 }
