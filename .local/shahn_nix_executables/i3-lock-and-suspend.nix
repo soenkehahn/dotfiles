b63cf06548c420a5c72932d4e2de37bb68244156
Sönke Hahn <soenkehahn@gmail.com>
2021-06-10 11:23:19 -0400

    Switch lock-screen and desktop background to black

--- .local/shahn_nix_executables/i3-lock-and-suspend.nix
+++ .local/shahn_nix_executables/i3-lock-and-suspend.nix
@@ -1,12 +1,13 @@
-with (import ./nixpkgs.nix).stable;
+with (import ./nixpkgs.nix);
+with (np "42aad3948cd" "1kml10ri1ka9z7n5z8i694l04ym2rczyg25avvn51y2yfcxjh85v");
 
 rustPlatform.buildRustPackage {
   name = "i3-lock-and-suspend";
   src = fetchFromGitHub {
     owner = "soenkehahn";
     repo = "i3-lock-and-suspend";
-    rev = "6d67b2a";
-    sha256 = "sha256:05554a93qkkbf2h35ffjy94466ygin0s00clg3mrpwnwg7by0vwl";
+    rev = "153c41da3143398765b8b8d79d70a1aec952c4b2";
+    sha256 = "sha256:00p2ygvq624gxcapn3ccxcilqiyy07ykvxm7lsq94z5r2847550p";
   };
-  cargoSha256 = "0jacm96l1gw9nxwavqi1x4669cg6lzy9hr18zjpwlcyb3qkw9z7f";
+  cargoSha256 = "1vha6mz5gj2ll4hgvdj1xjab5q3rlpdwdi3dyp07l33g3xwkq4wb";
 }
