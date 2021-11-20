c42db3e737eb8087406a15e08b21d76cc4218ad1
Sönke Hahn <soenkehahn@gmail.com>
2021-11-19 22:20:32 -0500

    Update nix installed packages

--- .local/shahn_nix_executables/default.nix
+++ .local/shahn_nix_executables/default.nix
@@ -2,22 +2,16 @@ with (import ./nixpkgs.nix);
 stable.buildEnv {
   name = "shahn_nix_executables";
   paths = (
-    (with (np "1cafe467b1b" "04qwnvx31x3d8mibyvjvj5n2y1cw367r3hyw5pdak0r91gzf1185"); [
-      # node 8
-    ]) ++
-    (with (np "1ac2c6b054a257ed7c849b7dea7ebcb7045e1f59" "1jz3vvl1fvkjgyhr4r22mglp5zld3zfnvmlxb2xgprgixz3pw6g9"); [
-      # node 10
-    ]) ++
-    (with (np "fccf069" "0if0n2gy0s46q078qg4xrzyq3fz1an888plgdp1hhs1bz7c8mh9x"); [
-    ]) ++
-    (with (np "42aad3948cd" "1kml10ri1ka9z7n5z8i694l04ym2rczyg25avvn51y2yfcxjh85v"); [
+    (with (np "4dc1154e" "1vr38m1nzwmad2r76i9rh1ccjrfzhyvbbzc10c2j4ys0m3yrr9qg"); [
       ag
       age
       blackbox
       cabal2nix
       docker-compose
       fd
+      ipfs
       ngrok
+      nix-query-tree-viewer
       nodejs
       nodePackages.js-beautify
       nodePackages.parcel-bundler
@@ -26,12 +20,6 @@ stable.buildEnv {
       pandoc
       terraform
       yarn
-    ]) ++
-    (with (np "34b37ad5" "09hsmbpiycyffjq7k49g8qav4fwlj6nrp9nzxpk5rm1rws8h9z61"); [
-      ipfs
-    ]) ++
-    (with (np "ef64a9a4" "0bj4nz6x04wc75rajyi29j64l6b88q2y5a1vlprrxlpyikklq68g"); [
-      nix-query-tree-viewer
     ]) ++
     [
       (import ./cargo-bump.nix)
