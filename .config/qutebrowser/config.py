aa70f119ca4b271e7b6d317c132b48ac07769fa3
Sönke Hahn <soenkehahn@gmail.com>
2022-06-20 12:46:02 -0400

    Switch to qutebrowser

---
+++ .config/qutebrowser/config.py
@@ -0,0 +1,8 @@
+c.fonts.default_size = "20pt"
+c.zoom.default = "200%"
+config.load_autoconfig()
+c.url.searchengines = {
+    "DEFAULT": "https://duckduckgo.com/?q={}",
+    "we": "https://en.wikipedia.org/w/index.php?title=Special%3ASearch&search={}",
+    "wd": "https://de.wikipedia.org/w/index.php?title=Spezial%3ASuche&search={}",
+}
