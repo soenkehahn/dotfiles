955cc12d496f8b15d1432d9bb9e50bf103bee399
Sönke Hahn <soenkehahn@gmail.com>
2022-07-05 12:46:57 -0400

    [qutebrowser] Tweak config

--- .config/qutebrowser/config.py
+++ .config/qutebrowser/config.py
@@ -1,8 +1,10 @@
 c.fonts.default_size = "20pt"
 c.zoom.default = "200%"
+c.editor.command = ["alacritty", "-e", "nvim", "{file}"]
 config.load_autoconfig()
 c.url.searchengines = {
     "DEFAULT": "https://duckduckgo.com/?q={}",
     "we": "https://en.wikipedia.org/w/index.php?title=Special%3ASearch&search={}",
     "wd": "https://de.wikipedia.org/w/index.php?title=Spezial%3ASuche&search={}",
+    "maps": "https://www.google.com/maps/?q={}",
 }
