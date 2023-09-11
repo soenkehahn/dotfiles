c3a952d30c3fb96a1d2df8168004b7f876d7207d
Sönke Hahn <soenkehahn@gmail.com>
2023-09-11 16:10:23 +0200

    Tweak home-manager config

--- .config/home-manager/flake.nix
+++ .config/home-manager/flake.nix
@@ -1,6 +1,4 @@
 {
-  description = "Home Manager configuration of shahn";
-
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     home-manager = {
@@ -9,18 +7,15 @@
     };
     jj = {
       url = "github:martinvonz/jj";
-      inputs = {
-        nixpkgs.follows = "nixpkgs";
-      };
+      inputs.nixpkgs.follows = "nixpkgs";
     };
     nil = {
       url = "github:oxalica/nil";
-      inputs = {
-        nixpkgs.follows = "nixpkgs";
-      };
+      inputs.nixpkgs.follows = "nixpkgs";
     };
     i3-pretty-tree = {
       url = "github:soenkehahn/i3-pretty-tree/sway";
+      inputs.nixpkgs.follows = "nixpkgs";
     };
   };
 
