207c17aa0c6019cc11d86a630811f3f783226403
Sönke Hahn <soenkehahn@gmail.com>
2024-03-14 20:48:16 -0700

    Add mirror-screen

--- .config/home-manager/commands.nix
+++ .config/home-manager/commands.nix
@@ -7,4 +7,12 @@
       git branch -vv | rg ': gone\]' | choose 0 | parallel -j1 'git branch -D'
     '';
   })
+  (pkgs.writeShellApplication {
+    name = "mirror-screen";
+    runtimeInputs = [ pkgs.wf-recorder ];
+    text = ''
+      export SDL_VIDEODRIVER=wayland
+      wf-recorder -c rawvideo -m sdl -f pipe:wayland-mirror --output eDP-1
+    '';
+  })
 ]
