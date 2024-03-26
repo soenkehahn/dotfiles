32cc16a12a55c11a8fdafaf1ce4a13a33e01063c
Sönke Hahn <soenkehahn@gmail.com>
2024-03-26 18:13:50 +0200

    Move `git-watch-tree` into nix

--- .config/home-manager/commands.nix
+++ .config/home-manager/commands.nix
@@ -15,4 +15,24 @@
       wf-recorder -c rawvideo -m sdl -f pipe:wayland-mirror --output eDP-1
     '';
   })
+  (pkgs.writeScriptBin
+    "git-watch-tree"
+    ''
+      if git rev-parse --show-toplevel
+      then
+        cd $(git rev-parse --show-toplevel)
+
+        tput rmam
+
+        while true ; do
+          export lines=$(tput lines)
+          find .git/ | rg -v '^.git/objects/' | \
+            entr -rcds 'git -c color.ui=always log --oneline --graph --decorate --remotes --branches | head -n $(expr $lines - 1)'
+        done
+      else
+        while true ; do
+          sleep 100
+        done
+      fi
+    '')
 ]
