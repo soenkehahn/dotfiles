c8c01b3fb73d7ae3cb1f29fbe10a4fd05e3de07e
Sönke Hahn <soenkehahn@gmail.com>
2025-10-02 22:19:32 -0400

    Update jj

--- .config/home-manager/flake.nix
+++ .config/home-manager/flake.nix
@@ -43,7 +43,7 @@
     };
     is-cached.url = "github:soenkehahn/is_cached";
     jj = {
-      url = "github:martinvonz/jj/v0.33.0";
+      url = "github:martinvonz/jj/v0.34.0";
       inputs.nixpkgs.follows = "nixpkgs";
     };
     coding = {
